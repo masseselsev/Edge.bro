2a395b79077c4668429dd3d2bef4aebb3c451d18
masseselsev <masseselsev@gmail.com>
2026-07-31 21:00:55 +0500

    chore: bump version to v1.1.1

--- payload_client/backend/main.py
+++ payload_client/backend/main.py
@@ -18,7 +18,7 @@ except ImportError:
 try:
     from version import VERSION
 except ImportError:
-    VERSION = "v1.1.0"
+    VERSION = "v1.1.1"
 
 app = FastAPI(title="Offline Technician Client", version=VERSION)
 
